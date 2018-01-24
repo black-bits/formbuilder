be4eb460e9ec1e16624ef5c5ddd5fa770fa7e202
Oliver Heck <mail@oliverheck.de>
2018-01-23 19:36:09 -0800

    Adds disabled and readonly property

--- src/Formbuilder.php
+++ src/Formbuilder.php
@@ -28,19 +28,19 @@ class Formbuilder
         return $this->toHtmlString(view('formbuilder::form-close'));
     }
 
-    public function text($name, $value, $label, $placeholder = "")
+    public function text($name, $value, $label, $placeholder = "", $readonly = false, $disabled = false)
     {
-        return $this->toHtmlString(view('formbuilder::form-text', compact([ 'name', 'value', 'label', 'placeholder' ])));
+        return $this->toHtmlString(view('formbuilder::form-text', compact([ 'name', 'value', 'label', 'placeholder', 'readonly', 'disabled' ])));
     }
 
-    public function email($name, $value, $label, $placeholder = "")
+    public function email($name, $value, $label, $placeholder = "", $readonly = false, $disabled = false)
     {
-        return $this->toHtmlString(view('formbuilder::form-email', compact([ 'name', 'value', 'label', 'placeholder' ])));
+        return $this->toHtmlString(view('formbuilder::form-email', compact([ 'name', 'value', 'label', 'placeholder', 'readonly', 'disabled' ])));
     }
 
-    public function password($name, $value, $label, $placeholder = "")
+    public function password($name, $value, $label, $placeholder = "", $readonly = false, $disabled = false)
     {
-        return $this->toHtmlString(view('formbuilder::form-password', compact([ 'name', 'value', 'label', 'placeholder' ])));
+        return $this->toHtmlString(view('formbuilder::form-password', compact([ 'name', 'value', 'label', 'placeholder', 'readonly', 'disabled' ])));
     }
     
     public function buttons($cancelAction = null, $cancelLabel = 'Cancel', $submitLabel = 'Save')
--- src/views/form-email.blade.php
+++ src/views/form-email.blade.php
@@ -1,6 +1,15 @@
 <div class='form-group {{ $errors->has($name) ? 'has-error' : '' }}'>
     <label for='{{ $name }}'>{{ $label }}</label>
-    <input type='email' class='form-control' id='{{ $name }}' name='{{ $name }}' placeholder='{{ $placeholder }}' value='{{ $value }}' />
+    <input
+            type='email'
+            class='form-control'
+            id='{{ $name }}'
+            name='{{ $name }}'
+            placeholder='{{ $placeholder }}'
+            value='{{ $value }}'
+            {{ $readonly ? 'readonly' : '' }}
+            {{ $disabled ? 'disabled' : '' }}
+    />
     @if ($errors->has($name))
         <span class="help-block">
             <strong>{{ $errors->first($name) }}</strong>
